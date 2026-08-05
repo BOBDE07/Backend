b1cb5b8bfcc602697884ff4f7c3c4b891f8f2d19
Nikhil Bobde <bobdenikhil53@gmail.com>
2026-08-05 10:23:37 +0530

    feat: add middleware for CORS and cookie parsing, implement server startup logic

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express" 
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express() 
+
+// use method ko middlewares mei use krte hai 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true ,
+
+})) 
+
+app.use(express.json({limit: "16kb"})) // json limit 
+
+app.use(express.urlencoded({extended: true , limit: "16kb"})) // extended ke andar object ke andar object de skte hai 
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+export { app }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,22 @@
 import dotenv from "dotenv"
 
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 dotenv.config({
     path: './env'
 })
 
-connectDB()
+connectDB() // database connected
+.then(() =>{
+    app.listen(process.env.PORT || 3000 ,() => {
+        console.log(`Server is running at port:${process.env.PORT} `);
+        
+    })
+}) 
+.catch((error) =>{
+    console.log("MONGODB connection failed !!!", error)
+})
 
 
 
